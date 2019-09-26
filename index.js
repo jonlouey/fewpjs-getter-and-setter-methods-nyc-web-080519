2e0397798e0283ea810131d0002a1ccc73c13f55
github username <github email address>
2019-09-26 17:19:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your Circle class here
+const pi = Math.PI
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  get diameter() {
+    return this.radius*2
+  }
+
+  set diameter(diameter) {
+    this.radius = diameter/2
+  }
+
+  get circumference() {
+    return (this.radius*2) * pi
+  }
+
+  set circumference(circumference) {
+    this.radius = circumference/(pi*2)
+  }
+
+  get area() {
+    return pi * (this.radius*this.radius )
+  }
+
+  set area(area) {
+    this.radius = Math.sqrt(area/pi)
+  }
+}
+
+//----------------------------- Notes --------------------------------------------
+
+//The get keyword turns a method into a 'pseudo-property', that is - it allows us to write a method that interacts like a property.
+//To change data, we have set.The set keyword allows us to write a method that interacts like a property being assigned a value. By adding it in conjunction with a get, we can create a 'reassignable' pseudo-property.
